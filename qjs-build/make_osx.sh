bb97de5059fb609bc0262b030669c397d8b1f388
johnche <johnche@tencent.com>
2020-11-18 10:22:04 +0800

    添加JS_STRICT_NAN_BOXING选项，并更改mac库的目录

--- qjs-build/make_osx.sh
+++ qjs-build/make_osx.sh
@@ -2,5 +2,5 @@ mkdir -p build && cd build
 cmake -GXcode ../
 cd ..
 cmake --build build --config Release
-mkdir -p ~/qjs/Lib/OSX/
-cp build/Release/libquickjs.a ~/qjs/Lib/OSX/
+mkdir -p ~/qjs/Lib/macOS/
+cp build/Release/libquickjs.a ~/qjs/Lib/macOS/
